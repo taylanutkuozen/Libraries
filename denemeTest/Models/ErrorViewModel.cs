526839da1b766adb940db8474cea66fabb593adf
taylanutkuozen <124128646+taylanutkuozen@users.noreply.github.com>
2023-11-04 07:20:36 +0300

    Delete denemeTest directory

--- denemeTest/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FluentValidation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
