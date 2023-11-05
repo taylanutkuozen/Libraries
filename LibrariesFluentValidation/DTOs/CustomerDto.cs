a886fa97b58cd420d818856ddd9a9605346dba09
Taylan Utku ÖZEN <taylanutku.ozen@gmail.com>
2023-11-05 21:29:30 +0300

    IncludeMembers ufnction

--- LibrariesFluentValidation/DTOs/CustomerDto.cs
+++ LibrariesFluentValidation/DTOs/CustomerDto.cs
@@ -7,8 +7,14 @@
         public string CustomerMail { get; set; }
         public int CustomerAge { get; set; }
         public string FullName { get; set; }
+        public string Number { get; set; }
+        public DateTime ValidDate { get; set; } //İsimleri birebir aynı olarak kullandık(IncludeMembers method)
+        /*
         public string CCNumber { get; set; }
         public DateTime CCValidDate { get; set; }
+        Flattening Manuel
+         */
+
         /*public string CreditCardNumber { get; set; } //Flattening otomatik yapabilmek için ilgili class ve ilgili property isimleri birleştirilir ve type olarak ilgili property type verilir.
         public DateTime CreditCardValidDate { get; set; }*/
     }
--- LibrariesFluentValidation/Mapping/CustomerProfile.cs
+++ LibrariesFluentValidation/Mapping/CustomerProfile.cs
@@ -7,12 +7,16 @@ namespace LibrariesFluentValidation.Mapping
     {
         public CustomerProfile()
         {
-            CreateMap<Customer, CustomerDto>().ReverseMap();//Bu metot generic olarak bir kaynak alır ve bir dönüştürecek. Kaynağın Customer bu kaynağı CustomerDto dönüştür.
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+            //Bu metot generic olarak bir kaynak alır ve bir dönüştürecek. Kaynağın Customer bu kaynağı CustomerDto dönüştür.
             //CreateMap<CustomerDto, Customer>();=ReverseMap metodu bu görevi gerçekleştirebiliyor.
-            CreateMap<Customer, CustomerDto>()
-                .ForMember(dest => dest.FullName, opt => opt.MapFrom(x => x.FullName2()))
-                .ForMember(dest=>dest.CCNumber,opt=>opt.MapFrom(x=>x.CreditCard.Number))
-                .ForMember(dest=>dest.CCValidDate,opt=>opt.MapFrom(x=>x.CreditCard.ValidDate));//Manuel olarak method property mapping(FullName)-Flattening(CCNumber,CCValidDate)
+            CreateMap<CreditCard, CustomerDto>();
+            //IncludeMembers için bir map işlemi gereklidir.
+            CreateMap<Customer, CustomerDto>().IncludeMembers(x=>x.CreditCard)
+                //IncludeMembers kullanabilmek için hem compleks type taki property isimleri hem DTO tarafındaki property isimleri aynı olmalıdır.
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(x => x.FullName2()));
+                /*.ForMember(dest=>dest.CCNumber,opt=>opt.MapFrom(x=>x.CreditCard.Number))
+                .ForMember(dest=>dest.CCValidDate,opt=>opt.MapFrom(x=>x.CreditCard.ValidDate));Manuel olarak method property mapping(FullName)-Flattening(CCNumber,CCValidDate)*/
             CreateMap<Customer, CustomerDtoTurkce>()
                 .ForMember(dest => dest.MusteriIsim, opt => opt.MapFrom(x => x.CustomerName))
                 .ForMember(dest => dest.Eposta, opt => opt.MapFrom(x => x.CustomerMail))
